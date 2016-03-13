dd6cbdeb40f698dfff9beb3ef7da706a8a2c3f01
jchbh <changhai.jiang@gmail.com>
2016-03-12 23:47:45 -0400

    fix require.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var wechat = require('./lib/wechat-enterprise');
 wechat.List = require('./lib/list');
-wechat.API = require('wechat-enterprise-api');
+wechat.API = require('skyline-wechat-enterprise-api');
 wechat.util = require('./lib/util');
 module.exports = wechat;
